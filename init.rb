a6b95ffbec75f4b2e154856364a67f690c4e62aa
Jacek Grzybowski <jgrzybowski@artegence.com>
2015-06-24 15:37:16 +0200

    first commit

---
+++ init.rb
@@ -0,0 +1,11 @@
+Redmine::Plugin.register :redmine_issue_tags do
+  name 'Redmine Issue Tags'
+  author 'Jacek Grzybowski'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'https://github.com/efigence/redmine_issue_tags'
+  author_url 'https://github.com/efigence'
+end
+
+ActionDispatch::Callbacks.to_prepare do
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# Load the Redmine helper
+require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
+
+# Set fixtures root
+ActiveSupport::TestCase.fixture_path=(File.expand_path("../fixtures",  __FILE__))
+ActiveSupport::TestCase.fixtures :all
