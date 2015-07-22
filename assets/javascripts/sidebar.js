cae8c51ca3e61379f2becd125a48249aaa7b4420
Jacek Grzybowski <jgrzybowski@artegence.com>
2015-07-22 17:11:32 +0200

    further refactor and autocomplete

--- assets/javascripts/sidebar.js
+++ assets/javascripts/sidebar.js
@@ -5,6 +5,7 @@
     init: function() {
       this.events.bindAll();
       this.selectizePrivateForm();
+      this.selectizePublicForm();
     },
 
     events: {
@@ -17,86 +18,125 @@
         this.tagDelete();
       },
 
-      privateSubmit: function() {
-        $('#add_private').click(function(e){
-          e.preventDefault();
-          $.ajax({
-            type: 'POST',
-            url: $(this).data('url'),
-            data: {
-              issue_id: $(this).data('issue-id'),
-              value: $('input#private_tag').val()
-            },
-            success: function(resp) {
-              if (resp.status === 'success') {
-                var arr = $.map(resp.tag_links, function(v) { return v });
-                $('#private-tags-container').html( arr.join(''));
+      helpers: {
+
+        submitHelper: function(type) {
+          $('#add_' + type).click(function(e){
+            e.preventDefault();
+            $.ajax({
+              type: 'POST',
+              url: $(this).data('url'),
+              data: {
+                issue_id: $(this).data('issue-id'),
+                value: $('input#' + type + '_tag').val()
+              },
+              success: function(resp) {
+                if (resp.status === 'success') {
+                  var arr = $.map(resp.tag_links, function(v) { return v });
+                  $('#' + type + '-tags-container').html( arr.join(''));
+                }
+              },
+              complete: function() {
+                $('#add-' + type + '-form').hide();
+                $('input#' + type + '_tag').val('');
+                $('#toggle-' + type + '-form').text( $('#toggle-' + type + '-form').data('add') );
               }
-            },
-            complete: function() {
-              $('#add-private-form').hide();
-              $('input#private_tag').val('');
-              $('#toggle-private-form').text( $('#toggle-private-form').data('add') );
+            });
+          });
+        },
+
+        toggleHelper: function(type) {
+          $('#toggle-' + type + '-form').click(function(e){
+            e.preventDefault();
+            var form = $('#add-' + type + '-form');
+            if (form.is(':visible')) {
+              $(this).text( $(this).data('add') );
+              form.hide();
+            } else {
+              $(this).text( $(this).data('cancel') );
+              form.show();
             }
           });
-        });
+        },
+
+        selectizeHelper: function(type) {
+
+          var addBtn = $('#add_' + type),
+              apiUrl = addBtn.data('api-url');
+
+          var clearOpts = function(selektize) {
+            $.each(selektize.options, function(k,v) {
+              if ( selektize.items.indexOf(k) === -1 ) {
+                selektize.removeOption(k);
+              }
+            });
+            selektize.refreshOptions();
+          }
+
+          var opts =  {
+            delimiter: ',',
+            plugins: ['enter_key_submit'],
+            valueField: 'name',
+            labelField: 'name',
+            searchField: 'name',
+            options: [],
+            loadThrottle: 600,
+            allowEmptyOption: true,
+
+            onInitialize: function () {
+              this.on('submit', function () {
+                addBtn.click();
+                this.clearOptions();
+              }, this)
+            },
+
+            create: function(input) {
+              return { name: input }
+            },
+
+            onItemAdd: function() {
+              clearOpts(this);
+            },
+
+            onBlur: function() {
+              clearOpts(this);
+            },
+
+            load: function(query, callback) {
+
+              if (!query.length) return callback();
+              $.ajax({
+                url: apiUrl,
+                type: 'GET',
+                dataType: 'json',
+                data: { name: query },
+                error: function() {
+                  callback();
+                },
+                success: function(res) {
+                  callback(res.tags);
+                }
+              });
+            }
+          }
+          return opts;
+        }
+      },
+
+      privateSubmit: function() {
+        this.helpers.submitHelper('private');
       },
 
       publicSubmit: function() {
-        $('#add_public').click(function(e){
-          e.preventDefault();
-          $.ajax({
-            type: 'POST',
-            url: $(this).data('url'),
-            data: {
-              issue_id: $(this).data('issue-id'),
-              value: $('input#public_tag').val()
-            },
-            success: function(resp) {
-              if (resp.status === 'success') {
-                var arr = $.map(resp.tag_links, function(v){
-                  return v
-                });
-                $('#public-tags-container').html( arr.join(''));
-              }
-            },
-            complete: function() {
-              $('#add-public-form').hide();
-              $('input#public_tag').val('');
-              $('#toggle-public-form').text( $('#toggle-public-form').data('add') );
-            }
-          });
-        });
+        this.helpers.submitHelper('public');
       },
 
       togglePrivate: function() {
-        $('#toggle-private-form').click(function(e){
-          e.preventDefault();
-          var form = $('#add-private-form');
-
-          if (form.is(':visible')) {
-            $(this).text( $(this).data('add') );
-            form.hide();
-          } else {
-            $(this).text( $(this).data('cancel') );
-            form.show();
-          }
-        });
+        this.helpers.toggleHelper('private');
       },
 
       togglePublic: function() {
-        $('#toggle-public-form').click(function(e){
-          e.preventDefault();
-          var form = $('#add-public-form');
-
-          if (form.is(':visible')) {
-            $(this).text( $(this).data('add') );
-            form.hide();
-          } else {
-            $(this).text( $(this).data('cancel') );
-            form.show();
-          }
-        });
+        this.helpers.toggleHelper('public');
       },
 
       tagDelete: function() {
@@ -119,71 +159,17 @@
     },
 
     selectizePrivateForm: function() {
-      $('input#private_tag').selectize({
-        delimiter: ',',
-        plugins: ['enter_key_submit'],
-        valueField: 'name',
-        labelField: 'name',
-        searchField: 'name',
-        options: [],
-        loadThrottle: 600,
-        allowEmptyOption: true,
+      $('input#private_tag').selectize(
+        this.events.helpers.selectizeHelper('private')
+      );
+    },
 
-        onInitialize: function (foo) {
-          this.on('submit', function () {
-            $('#add_private').click();
-            this.clearOptions();
-          }, this)
-        },
-
-        create: function(input) {
-          return { name: input }
-        },
-
-        onItemAdd: function() {
-          self = this;
-          var clearOpts = function(s) {
-            $.each(self.options, function(k,v) {
-              if ( self.items.indexOf(k) === -1 ) {
-                self.removeOption(k);
-              }
-            });
-
-            self.refreshOptions();
-          }
-          clearOpts(this);
-        },
-
-        load: function(query, callback) {
-          if (!query.length) return callback();
-          $.ajax({
-            url: '/tags_api/private',
-            type: 'GET',
-            dataType: 'json',
-            data: { name: query },
-            error: function() {
-                callback();
-            },
-            success: function(res) {
-                callback(res.tags);
-            }
-          });
-        }
-      });
-
-      $('input#public_tag').selectize({
-        delimiter: ',',
-        persist: false,
-        create: function(input) {
-            return {
-                value: input,
-                text: input
-            }
-        }
-      });
+    selectizePublicForm: function() {
+      $('input#public_tag').selectize(
+        this.events.helpers.selectizeHelper('public')
+      );
     }
   }
-
 
   $(function(){
 
