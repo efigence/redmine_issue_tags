8cc50f080e1e500bceff92c3c6dc848982447124
Jacek Grzybowski <jgrzybowski@artegence.com>
2015-07-23 17:32:22 +0200

    tags on issue creation

--- assets/javascripts/new_issue_tags.js
+++ assets/javascripts/new_issue_tags.js
@@ -1,49 +1,90 @@
-$(function(){
+(function(){
+  var issueTags = {
 
-  $('#issue_private_tags').selectize({
-    delimiter: ',',
-    valueField: 'name',
-    labelField: 'name',
-    searchField: 'name',
-    options: [],
-    loadThrottle: 600,
-    allowEmptyOption: true,
-
-    onInitialize: function () {
-      this.on('submit', function () {
-        addBtn.click();
-        this.clearOptions();
-      }, this)
+    init: function() {
+      this.selectizePublicTags();
+      this.selectizePrivateTags();
     },
 
-    create: function(input) {
-      return { name: input }
+    selectizePublicTags: function() {
+      this.selectizeForm( $('#issue_public_tags') );
     },
 
-    onItemAdd: function() {
-      // clearOpts(this);
+    selectizePrivateTags: function() {
+      this.selectizeForm( $('#issue_private_tags') );
     },
 
-    onBlur: function() {
-      // clearOpts(this);
+    selectizeForm: function(context) {
+      var opts = this.selectizeOpts(context);
+      context.selectize(opts);
     },
 
-    load: function(query, callback) {
+    selectizeOpts: function(caller) {
 
-      if (!query.length) return callback();
-      $.ajax({
-        url: $('#issue_private_tags').data('api-url'),
-        type: 'GET',
-        dataType: 'json',
-        data: { name: query },
-        error: function() {
-          callback();
+      var apiUrl = caller.data('api-url');
+
+      var clearOpts = function(selektize) {
+        $.each(selektize.options, function(k,v) {
+          if ( selektize.items.indexOf(k) === -1 ) {
+            selektize.removeOption(k);
+          }
+        });
+        selektize.refreshOptions();
+      }
+
+      var opts = {
+        delimiter: ',',
+        valueField: 'name',
+        labelField: 'name',
+        searchField: 'name',
+        options: [],
+        loadThrottle: 600,
+        allowEmptyOption: true,
+
+        onInitialize: function () {
+          this.on('submit', function () {
+            addBtn.click();
+            this.clearOptions();
+          }, this)
         },
-        success: function(res) {
-          callback(res.tags);
+
+        create: function(input) {
+          return { name: input }
+        },
+
+        onItemAdd: function() {
+          clearOpts(this);
+        },
+
+        onBlur: function() {
+          clearOpts(this);
+        },
+
+        load: function(query, callback) {
+
+          if (!query.length) return callback();
+          $.ajax({
+            url: apiUrl,
+            type: 'GET',
+            dataType: 'json',
+            data: { name: query },
+            error: function() {
+              callback();
+            },
+            success: function(res) {
+              callback(res.tags);
+            }
+          });
         }
-      });
+      }
+      return opts;
     }
+  }
+
+  $(function(){
+
+    issueTags.init();
+
   });
 
-});
+})();
